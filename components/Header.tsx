1ed4db96a32af458e931e59b328a8496915e0ce8
Cole <garcia.ccole@gmail.com>
2024-04-16 07:59:48 +0800

    initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between py-4 w-11/12 m-auto md:w-10/12">
+      <Image
+        src={"/images/logo.png"}
+        alt=""
+        width={0}
+        height={0}
+        className="w-28 "
+        sizes="100vw"
+      />
+      <div className="hidden md:flex align-middle items-center">
+        <ul className="flex">
+          <li className="mr-8 text-sm">Plants</li>
+          <li className="text-sm">About</li>
+        </ul>
+      </div>
+      <div className="flex items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          className="mr-4"
+        >
+          <path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path>
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          className="mr-4"
+        >
+          <path d="M12 2A10.13 10.13 0 0 0 2 12a10 10 0 0 0 4 7.92V20h.1a9.7 9.7 0 0 0 11.8 0h.1v-.08A10 10 0 0 0 22 12 10.13 10.13 0 0 0 12 2zM8.07 18.93A3 3 0 0 1 11 16.57h2a3 3 0 0 1 2.93 2.36 7.75 7.75 0 0 1-7.86 0zm9.54-1.29A5 5 0 0 0 13 14.57h-2a5 5 0 0 0-4.61 3.07A8 8 0 0 1 4 12a8.1 8.1 0 0 1 8-8 8.1 8.1 0 0 1 8 8 8 8 0 0 1-2.39 5.64z"></path>
+          <path d="M12 6a3.91 3.91 0 0 0-4 4 3.91 3.91 0 0 0 4 4 3.91 3.91 0 0 0 4-4 3.91 3.91 0 0 0-4-4zm0 6a1.91 1.91 0 0 1-2-2 1.91 1.91 0 0 1 2-2 1.91 1.91 0 0 1 2 2 1.91 1.91 0 0 1-2 2z"></path>
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          className="mr-4"
+        >
+          <path d="M21 4H2v2h2.3l3.28 9a3 3 0 0 0 2.82 2H19v-2h-8.6a1 1 0 0 1-.94-.66L9 13h9.28a2 2 0 0 0 1.92-1.45L22 5.27A1 1 0 0 0 21.27 4 .84.84 0 0 0 21 4zm-2.75 7h-10L6.43 6h13.24z"></path>
+          <circle cx="10.5" cy="19.5" r="1.5"></circle>
+          <circle cx="16.5" cy="19.5" r="1.5"></circle>
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          className="md:hidden"
+        >
+          <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path>
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ components/Hero.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div className="w-11/12 m-auto md:w-10/12">
+      <div className=" flex mb-8">
+        <div className="md:flex-1">
+          <h1 className="text-3xl md:text-5xl font-bold pt-12 pb-8 w-2/3 font-playfair ">
+            Elevate Your Space with Houseplant
+          </h1>
+          <button className="bg-black text-white py-2 px-7 rounded-full mb-12">
+            Shop now
+          </button>
+          <p className="mb-6 md:w-1/3">
+            Beautiful living greenery for homes and offices
+          </p>
+          <p className="text-xs mb-4">We've been mentioned in the press</p>
+          <div className="flex gap-4 py-4 flex-col md:flex-row items-center">
+            <img src="/images/Bloomberg.png" alt="" className="mr-8" />
+            <img src="/images/Forbes.png" alt="" />
+          </div>
+        </div>
+        <div className="md:w-[45%]">
+          <Image
+            src={"/images/image-3.jpg"}
+            alt=""
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-full object-cover h-[500px] object-bottom"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col gap-8 md:flex-row">
+        <div className="md:w-[40%]">
+          <p className="text-sm text-center md:text-start">
+            Each houseplant set is handled quickly and safely before being sent
+            to your destination in specially designed insulated packaging
+          </p>
+        </div>
+        <div className="md:w-flex-1">
+          <p className="font-bold text-center mb-4 md:text-start md:w-2/3">
+            Learn how we take care of your plant at every stage of its journey
+            from our greenhouse to your home
+          </p>
+          <div className="flex justify-center md:justify-start">
+            <p className="mr-4 text-sm">LEARN MORE</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 15v-4H7v-2h5V7l5 5-5 5z"></path>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
